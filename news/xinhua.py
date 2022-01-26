57fa908957b0de5fbba672df551e05582a983b62
55229lwj <83404885+55229lwj@users.noreply.github.com>
2022-01-26 15:55:55 +0800

    Update xinhua.py
    获取新华网热榜新闻

--- news/xinhua.py
+++ news/xinhua.py
@@ -1,22 +1,22 @@
 import requests
 from lxml import etree
-import pymysql
+import pymysql  # 提交数据库用
 import time
 url = "http://xinhuanet.com"
 resp = requests.get(url)
 html = resp.text.encode('raw_unicode_escape').decode()
-connect = pymysql.Connect(
-    host='localhost',
+connect = pymysql.Connect(     # 连接数据库
+    host='host',
     port=3306,
     user='root',
-    passwd='root',
-    db='lab',
+    passwd='your password',
+    db='db',
     charset='utf8'
 )
 cursor = connect.cursor()
 tree = etree.HTML(html)
-top = tree.xpath('/html/body/div[10]/div/div/div[2]/div/div[2]/div/div[@class="tit"]')
-for divs in top:
+top = tree.xpath('/html/body/div[10]/div/div/div[2]/div/div[2]/div/div[@class="tit"]')   # 解析
+for divs in top:        # 持久化
     news = divs.xpath('./a/text()')[0]
     urls = divs.xpath('./a/@href')[0]  # /html/body/div[10]/div/div/div[2]/div/div[2]/div[1]/div[1]/a
     print(news, urls)
